16929c0ebd6a8720e4e528dc2767df27423459f2
bibin thomas <bibinthomas951@gmail.com>
2023-02-08 00:36:50 +0530

    added home page

---
+++ src/pages/Home/Social.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/bibinthomas951/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://github.com/bibinthomas123"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/bibin-thomas-1962a2225/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
