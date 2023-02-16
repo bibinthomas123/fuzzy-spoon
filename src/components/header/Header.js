32f7701af6cdaf5ce615ac397ccd183655b38320
bibin thomas <bibinthomas951@gmail.com>
2023-02-16 17:28:49 +0530

    fuck 3d

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,7 +11,7 @@ function Header() {
         {/* <NavLink to={"/"} className="nav__logo">
           Bibin Thomas
         </NavLink> */}
-        <MusicPlayer/>
+        <MusicPlayer />
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
@@ -76,14 +76,11 @@ function Header() {
             </li>
           </ul>
 
-          <i
-            className="uil uil-times nav__close"
-            onClick={() => showMenu(!Toggle)}
-          />
+          <i class="bx bx-x nav__close" onClick={() => showMenu(!Toggle)} />
         </div>
 
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-          <i className="uil uil-apps" />
+          <i class="bx bx-grid-alt"></i>
         </div>
       </nav>
     </header>
