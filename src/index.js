0881e38638cd16aed585a8a77fc65af659e4783d
bibin thomas <bibinthomas951@gmail.com>
2023-03-19 00:35:17 +0530

    sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Customcursor from "./components/Custom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://d1931a06b5434974b83e163ee5221261@o4504202904731648.ingest.sentry.io/4504861071704064",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
